245baf243e5bb66425238ebb3f7f2b5585577731
tjs123 <249502280@qq.com>
2015-04-01 15:09:19 +0800

    信托产品代码上传

---
+++ webplat/src/main/java/com/tjs/admin/xintuo/service/impl/ProductXtcpServiceImpl.java
@@ -0,0 +1,51 @@
+package com.tjs.admin.xintuo.service.impl;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.tjs.admin.xintuo.dao.ProductXtcpMapper;
+import com.tjs.admin.xintuo.model.ProductXtcp;
+import com.tjs.admin.xintuo.service.IProductXtcpService;
+
+@Service
+public class ProductXtcpServiceImpl  implements IProductXtcpService {
+
+	@Resource
+    private ProductXtcpMapper productXtcpMapper;
+
+	@Override
+	public int countProductXtcp(ProductXtcp productXtcp) {
+		return productXtcpMapper.countProductXtcp(productXtcp);
+	}
+
+	@Override
+	public int insertProductXtcp(ProductXtcp productXtcp) {
+		return productXtcpMapper.insertProductXtcp(productXtcp);		
+	}
+
+	@Override
+	public int updateProductXtcp(ProductXtcp productXtcp) {
+		return productXtcpMapper.updateProductXtcp(productXtcp);
+	}
+
+	@Override
+	public void deleteProductXtcp(Long xtcpId) {
+		productXtcpMapper.deleteProductXtcp(xtcpId);
+	}
+
+	@Override
+	public ProductXtcp findByProductXtcpId(Long xtcpId) {
+		return productXtcpMapper.findByProductXtcpId(xtcpId);
+	}
+
+	@Override
+	public List<ProductXtcp> selectProductXtcp() {		
+		return productXtcpMapper.selectProductXtcp();
+	}
+		
+	
+
+}
