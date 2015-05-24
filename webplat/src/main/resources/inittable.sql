6c7d2a99612170acabc175defd65bcff58fc3711
tjs123 <249502280@qq.com>
2015-05-24 19:10:52 +0800

    提交信托脚本

--- webplat/src/main/resources/inittable.sql
+++ webplat/src/main/resources/inittable.sql
@@ -29,32 +29,24 @@ create table t_web_log
    primary key (id)
 ) DEFAULT CHARSET=utf8 comment='web日志';
 
-DROP TABLE IF EXISTS `tjs_product_xtgs`;
-CREATE TABLE `tjs_product_xtgs` (
-  `xtgs_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '流水号',
-  `xtgs_code` varchar(200) DEFAULT NULL COMMENT '公司编码',
-  `xtgs_log` varchar(500) DEFAULT NULL COMMENT '公司LOGO',
-  `xgts_zhname` varchar(500) DEFAULT NULL COMMENT '公司中文名称',
-  `xgts_enname` varchar(200) DEFAULT NULL COMMENT '公司英文名称 ',
-  `xgts_splname` varchar(200) DEFAULT NULL COMMENT '公司简称',
-  `xgts_createdate` date DEFAULT NULL COMMENT '成立日期',
-  `xgts_zczb` float(18,2) DEFAULT NULL COMMENT '注册资本',
-  `xgts_city` varchar(200) DEFAULT NULL COMMENT '所在城市',
-  `xgts_gsz` varchar(200) DEFAULT NULL COMMENT '董事长',
-  `xgts_zjl` varchar(200) DEFAULT NULL COMMENT '总经理',
-  `xgts_frdb` varchar(200) DEFAULT NULL COMMENT '法人代表',
-  `xgts_dgd` varchar(200) DEFAULT NULL COMMENT '大股东',
-  `xgts_ss` varchar(45) DEFAULT NULL COMMENT '是否上市',
-  `xgts_gsxz` varchar(45) NOT NULL COMMENT '公司性质',
-  `xgts_zcglgm` varchar(2000) DEFAULT NULL COMMENT '公司管理规模',
-  `xgts_gsjj` varchar(2000) DEFAULT NULL COMMENT '公司简介',
-  `xgts_status` varchar(5) DEFAULT NULL COMMENT '状态',
-  `xgts_tjw` varchar(5) DEFAULT NULL COMMENT '推荐位',
-  `xgts_tjno` bigint(20) DEFAULT NULL COMMENT '状态',
-  `xgts_pjsyl` decimal(10,2) DEFAULT NULL COMMENT '平均收益率',
-  `xgts_cpdfbl` decimal(10,2) DEFAULT NULL COMMENT '产品兑付比例',
-  PRIMARY KEY (`xtgs_id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='信托公司';
+
+DROP TABLE IF EXISTS `tjs_order`;
+CREATE TABLE `tjs_order` (
+  `order_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '订单号',
+  `order_user_id` bigint(20) DEFAULT NULL COMMENT '订单提交人ID',
+  `order_user_name` varchar(200) DEFAULT NULL COMMENT '订单人名称',
+  `order_telphone` varchar(45) DEFAULT NULL COMMENT '订单人电话',
+  `order_product_id` bigint(20) DEFAULT NULL COMMENT '订单产品ID',
+  `order_product_name` varchar(500) DEFAULT NULL COMMENT '订单产品名称',
+  `order_product_type` varchar(10) DEFAULT NULL COMMENT '订单产品类型',
+  `order_operate_status` varchar(45) DEFAULT NULL COMMENT '订单处理状态',
+  `order_operate_ID` bigint(20) DEFAULT NULL COMMENT '订单处理人ID',
+  `order_operate_name` varchar(200) DEFAULT NULL COMMENT '订单处理人',
+  `order_create_date` datetime DEFAULT NULL COMMENT '订单创建时间',
+  `order_operate_date` datetime DEFAULT NULL COMMENT '订单处理时间',
+  `order_remark` varchar(2000) DEFAULT NULL COMMENT '备注',
+  PRIMARY KEY (`order_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 COMMENT='预定信息表';
 
 DROP TABLE IF EXISTS `tjs_product_xtcp`;
 CREATE TABLE `tjs_product_xtcp` (
@@ -72,46 +64,57 @@ CREATE TABLE `tjs_product_xtcp` (
   `xtcp_dyl` varchar(200) DEFAULT NULL COMMENT '抵押率',
   `xtcp_tzly` varchar(200) DEFAULT NULL COMMENT '投资领域',
   `xtcp_dyw` varchar(200) DEFAULT NULL COMMENT '抵押物',
-  `xtcp_sysm` varchar(200) DEFAULT NULL COMMENT '收益说明',
+  `xtcp_sysm` varchar(4000) DEFAULT NULL COMMENT '收益说明',
   `xtcp_mjzh` varchar(500) DEFAULT NULL COMMENT '募集进度/账号',
-  `xtcp_rzf` varchar(100) DEFAULT NULL COMMENT '融资方',
+  `xtcp_rzf` varchar(4000) DEFAULT NULL COMMENT '融资方',
   `xtcp_zjyt` varchar(500) DEFAULT NULL COMMENT '资金用途',
   `xtcp_hkly` varchar(200) DEFAULT NULL COMMENT '还款来源',
   `xtcp_fkcs` varchar(1000) DEFAULT NULL COMMENT '风控措施',
-  `xtcp_zcglr` varchar(100) DEFAULT NULL COMMENT '资产管理人',
+  `xtcp_zcglr` varchar(4000) DEFAULT NULL COMMENT '资产管理人',
   `xtcp_dp` varchar(1000) DEFAULT NULL COMMENT '点评',
   `xtcp_hd` varchar(1000) DEFAULT NULL COMMENT '活动',
   `xtcp_status` varchar(45) DEFAULT NULL COMMENT '状态',
   `xtcp_tjw` varchar(45) DEFAULT NULL COMMENT '推荐位',
   `xtcp_tjno` bigint(20) DEFAULT NULL COMMENT '推荐序号',
-  `xtcp_createDate` date DEFAULT NULL COMMENT '产品上架时间',
-  `xtcp_modifyDate` date DEFAULT NULL COMMENT '产品修改时间',
+  `xtcp_createDate` datetime DEFAULT NULL COMMENT '产品上架时间',
+  `xtcp_modifyDate` datetime DEFAULT NULL COMMENT '产品修改时间',
   `xtcp_userId` varchar(50) DEFAULT NULL COMMENT '创建人Id ',
   `xtcp_username` varchar(50) DEFAULT NULL COMMENT '创建人name',
   `xtcp_validstatus` varchar(50) DEFAULT '1' COMMENT '有效状态1：有效，2：无效',
   `xtcp_area` varchar(100) DEFAULT NULL COMMENT '所在区域',
   `xtcp_city` varchar(100) DEFAULT NULL COMMENT '所在城市',
+  `xtcp_splname` varchar(500) DEFAULT NULL COMMENT '产品简称',
+  `xtcp_tzfs` varchar(100) DEFAULT NULL COMMENT '投资方式',
   PRIMARY KEY (`xtcp_id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='信托产品';
 
 
-
-
-CREATE TABLE `tjs_order` (
-  `order_id` bigint(20) NOT NULL COMMENT '订单号',
-  `order_user_id` bigint(20) DEFAULT NULL COMMENT '订单提交人ID',
-  `order_user_name` varchar(200) DEFAULT NULL COMMENT '订单人名称',
-  `order_telphone` varchar(45) DEFAULT NULL COMMENT '订单人电话',
-  `order_product_id` bigint(20) DEFAULT NULL COMMENT '订单产品ID',
-  `order_product_name` varchar(500) DEFAULT NULL COMMENT '订单产品名称',
-  `order_product_type` varchar(10) DEFAULT NULL COMMENT '订单产品类型',
-  `order_operate_status` varchar(45) DEFAULT NULL COMMENT '订单处理状态',
-  `order_operate_ID` bigint(20) DEFAULT NULL COMMENT '订单处理人ID',
-  `order_operate_name` varchar(200) DEFAULT NULL COMMENT '订单处理人',
-  `order_create_date` date DEFAULT NULL COMMENT '订单创建时间',
-  `order_operate_date` date DEFAULT NULL COMMENT '订单处理时间',
-  PRIMARY KEY (`order_id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='预定信息表';
+DROP TABLE IF EXISTS `tjs_product_xtgs`;
+CREATE TABLE `tjs_product_xtgs` (
+  `xtgs_id` bigint(20) NOT NULL AUTO_INCREMENT COMMENT '流水号',
+  `xtgs_code` varchar(200) DEFAULT NULL COMMENT '公司编码',
+  `xtgs_log` varchar(500) DEFAULT NULL COMMENT '公司LOGO',
+  `xgts_zhname` varchar(500) DEFAULT NULL COMMENT '公司中文名称',
+  `xgts_enname` varchar(200) DEFAULT NULL COMMENT '公司英文名称 ',
+  `xgts_splname` varchar(200) DEFAULT NULL COMMENT '公司简称',
+  `xgts_createdate` date DEFAULT NULL COMMENT '成立日期',
+  `xgts_zczb` decimal(18,2) DEFAULT NULL COMMENT '注册资本',
+  `xgts_city` varchar(200) DEFAULT NULL COMMENT '所在城市',
+  `xgts_gsz` varchar(200) DEFAULT NULL COMMENT '董事长',
+  `xgts_zjl` varchar(200) DEFAULT NULL COMMENT '总经理',
+  `xgts_frdb` varchar(200) DEFAULT NULL COMMENT '法人代表',
+  `xgts_dgd` varchar(200) DEFAULT NULL COMMENT '大股东',
+  `xgts_ss` varchar(45) DEFAULT NULL COMMENT '是否上市',
+  `xgts_gsxz` varchar(45) NOT NULL COMMENT '公司性质',
+  `xgts_zcglgm` varchar(2000) DEFAULT NULL COMMENT '公司管理规模',
+  `xgts_gsjj` varchar(2000) DEFAULT NULL COMMENT '公司简介',
+  `xgts_status` varchar(5) DEFAULT NULL COMMENT '状态',
+  `xgts_tjw` varchar(5) DEFAULT NULL COMMENT '推荐位',
+  `xgts_tjno` bigint(20) DEFAULT NULL COMMENT '状态',
+  `xgts_pjsyl` decimal(10,2) DEFAULT NULL COMMENT '平均收益率',
+  `xgts_cpdfbl` decimal(10,2) DEFAULT NULL COMMENT '产品兑付比例',
+  PRIMARY KEY (`xtgs_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='信托公司';
 
 DROP TABLE IF EXISTS `pe_company`;
 
@@ -261,47 +264,6 @@ CREATE TABLE `pe_product_net` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8 COMMENT='私募产品净值表';
 
-
-ALTER TABLE `tjs_product_xtcp` 
-CHANGE COLUMN `xtcp_sysm` `xtcp_sysm` VARCHAR(4000) NULL DEFAULT NULL COMMENT '收益说明' ;
-
-
-ALTER TABLE `tjs_product_xtcp` 
-CHANGE COLUMN `xtcp_zcglr` `xtcp_zcglr` VARCHAR(4000) NULL DEFAULT NULL COMMENT '资产管理人' ;
-
-
-ALTER TABLE `tjs_product_xtcp` 
-ADD COLUMN `xtcp_splname` VARCHAR(500) NULL AFTER `xtcp_city`;
-
-
-ALTER TABLE `tjs_product_xtcp` 
-CHANGE COLUMN `xtcp_splname` `xtcp_splname` VARCHAR(500) NULL DEFAULT NULL COMMENT '产品简称' ,
-ADD COLUMN `xtcp_tzfs` VARCHAR(100) NULL COMMENT '投资方式' AFTER `xtcp_splname`;
-
-
-ALTER TABLE `tjs_product_xtcp` 
-CHANGE COLUMN `xtcp_rzf` `xtcp_rzf` VARCHAR(4000) NULL DEFAULT NULL COMMENT '融资方' ;
-
-
-ALTER TABLE `tjs_order` 
-ADD COLUMN `order_remark` VARCHAR(2000) NULL COMMENT '备注' AFTER `order_operate_date`;
-
-ALTER TABLE `tjs_order` 
-CHANGE COLUMN `order_id` `order_id` BIGINT(20) NOT NULL AUTO_INCREMENT COMMENT '订单号' ;
-
-
-ALTER TABLE `tjs_product_xtgs` 
-CHANGE COLUMN `xgts_zczb` `xgts_zczb` DECIMAL(18,2) NULL DEFAULT NULL COMMENT '注册资本' ;
-
-
-ALTER TABLE `tjs_order` 
-CHANGE COLUMN `order_create_date` `order_create_date` DATETIME NULL DEFAULT NULL COMMENT '订单创建时间' ,
-CHANGE COLUMN `order_operate_date` `order_operate_date` DATETIME NULL DEFAULT NULL COMMENT '订单处理时间' ;
-
-
-ALTER TABLE `tjs_product_xtcp` 
-CHANGE COLUMN `xtcp_createDate` `xtcp_createDate` DATETIME NULL DEFAULT NULL COMMENT '产品上架时间' ,
-CHANGE COLUMN `xtcp_modifyDate` `xtcp_modifyDate` DATETIME NULL DEFAULT NULL COMMENT '产品修改时间' ;
 
 
 
