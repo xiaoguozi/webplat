0dbbaf850f4ebd1519b794afd32321d8cbe80d36
duanyujun <duanyujun@gmail.com>
2015-06-30 14:31:08 +0800

    充值成功后台代码提交

--- webplat/src/main/java/com/tjs/web/zhifu/controller/ZhifuController.java
+++ webplat/src/main/java/com/tjs/web/zhifu/controller/ZhifuController.java
@@ -3,6 +3,7 @@ package com.tjs.web.zhifu.controller;
 import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import javax.annotation.Resource;
@@ -19,10 +20,16 @@ import com.tjs.admin.model.User;
 import com.tjs.admin.model.UserInfo;
 import com.tjs.admin.service.UserInfoService;
 import com.tjs.admin.service.UserService;
+import com.tjs.admin.zhifu.controller.CustomerFundCtrlModel;
+import com.tjs.admin.zhifu.model.CustomerFund;
+import com.tjs.admin.zhifu.model.FundRecord;
 import com.tjs.admin.zhifu.model.Recharge;
+import com.tjs.admin.zhifu.service.ICustomerFund;
 import com.tjs.admin.zhifu.service.IRecharge;
 import com.tjs.admin.zhifu.zfenum.RechargeFundTypeEnum;
+import com.tjs.admin.zhifu.zfenum.RechargeStatusEnum;
 import com.tjs.core.zhifu.YeepayService;
+import com.tjs.web.zhifu.service.IZhifuService;
 
 @Controller
 @RequestMapping(value = "/web/userCenter/zhifu")
@@ -36,6 +43,13 @@ public class ZhifuController {
 	
 	@Resource
 	private UserInfoService userInfoService;
+	
+	@Resource
+    private IZhifuService zhifuService;
+	
+	@Resource
+	private ICustomerFund customerFundService;
+	
 	
 	@Autowired  
 	private  HttpServletRequest request; 
@@ -58,7 +72,7 @@ public class ZhifuController {
 		insertRecharge(request, recharge);
 		
 		String p0_Cmd           = "Buy";
-		String p2_Order         = "";
+		String p2_Order         = String.valueOf(recharge.getRechangeId());
 		String p3_Amt           = rechargeAmount;
 		String p4_Cur           = "CNY";
 		String p5_Pid           = "productname";
@@ -95,6 +109,51 @@ public class ZhifuController {
 	
 	@RequestMapping("/callback")
     public String callback(ZhifuModel zhifuModel, Model model) {
+		Subject subject = SecurityUtils.getSubject();
+		String username = (String)subject.getPrincipal();
+		User user = userService.selectByUsername(username);
+		UserInfo userInfo = userInfoService.findUserInfoByUserId(user.getId());
+		
+		//1、充值流水
+		String orderId = zhifuModel.getR6_Order();
+		Recharge recharge = rechargeService.findByRechargeId(Long.valueOf(orderId));
+		recharge.setStatus(RechargeStatusEnum.SUCCESS.getIntegerKey());
+		
+		//查询用户个人账户
+		CustomerFundCtrlModel customerFundCtrlModel = new CustomerFundCtrlModel();
+		
+		List<CustomerFund> lstCustomerFund = customerFundService.selectCustomerFund(customerFundCtrlModel);
+		CustomerFund customerFund = null;
+		if(lstCustomerFund!=null && lstCustomerFund.size()>0){
+			customerFund = lstCustomerFund.get(0);
+		}else{
+			CustomerFund cNewCustomerFund = new CustomerFund();
+			cNewCustomerFund.setCustomerId(user.getId());
+			cNewCustomerFund.setUsebleFund(BigDecimal.ZERO);
+			customerFundService.insertCustomerFund(cNewCustomerFund);
+			customerFund = cNewCustomerFund;
+		}
+		
+		//2、账户
+		BigDecimal usableAmount = null;
+		if(customerFund.getUsebleFund()==null){
+			usableAmount = new BigDecimal(zhifuModel.getR3_Amt());
+		}else{
+			usableAmount = customerFund.getUsebleFund().add(new BigDecimal(zhifuModel.getR3_Amt()));
+		}
+		customerFund.setUsebleFund(usableAmount);
+		
+		//3、资金流水
+		FundRecord fundRecord = new FundRecord();
+		fundRecord.setAmount(new BigDecimal(zhifuModel.getR3_Amt()));
+		fundRecord.setBusinessId(Long.valueOf(orderId));
+		fundRecord.setCreateBy(userInfo.getName()==null?username:userInfo.getName());
+		fundRecord.setCreateTime(Calendar.getInstance().getTime());
+		fundRecord.setCustomerId(user.getId());
+		fundRecord.setRecordDesc("充值");
+		fundRecord.setUsableAmount(usableAmount);
+		
+		zhifuService.callbackUpdate(recharge, fundRecord, customerFund);
 		
 		return "web/zhifu/callback";
 	}
---
+++ webplat/src/main/java/com/tjs/web/zhifu/service/impl/ZhifuService.java
@@ -0,0 +1,34 @@
+package com.tjs.web.zhifu.service.impl;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.tjs.admin.zhifu.model.CustomerFund;
+import com.tjs.admin.zhifu.model.FundRecord;
+import com.tjs.admin.zhifu.model.Recharge;
+import com.tjs.admin.zhifu.service.ICustomerFund;
+import com.tjs.admin.zhifu.service.IFundRecord;
+import com.tjs.admin.zhifu.service.IRecharge;
+import com.tjs.web.zhifu.service.IZhifuService;
+
+@Service
+public class ZhifuService implements IZhifuService {
+	
+	@Resource
+	private IRecharge rechargeService;
+	
+	@Resource
+	private IFundRecord fundRecordService;
+	
+	@Resource
+	private ICustomerFund customerFundService;
+
+	@Override
+	public void callbackUpdate(Recharge recharge, FundRecord fundRecord, CustomerFund customerFund) {
+		rechargeService.updateRecharge(recharge);
+		fundRecordService.insertFundRecord(fundRecord);
+		customerFundService.updateCustomerFund(customerFund);
+	}
+
+}
